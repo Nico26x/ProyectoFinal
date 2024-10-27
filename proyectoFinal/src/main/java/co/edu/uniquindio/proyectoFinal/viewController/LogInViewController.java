0f6b72ff5ab009f428989851ae615f62108c60bd
karenlara1 <karena.larab@uqvirtual.edu.co>
2024-10-27 14:27:11 -0500

    Commit corrección LogIn y creación paquete ViewController

---
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyectoFinal/viewController/LogInViewController.java
@@ -0,0 +1,30 @@
+package co.edu.uniquindio.proyectoFinal.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
+public class LogInViewController {
+
+    LogInViewController logInViewController;
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void ingresarUsuario(ActionEvent event) {
+
+    }
+
+}
--- proyectoFinal/src/main/java/module-info.java
+++ proyectoFinal/src/main/java/module-info.java
@@ -1,16 +1,9 @@
 module co.edu.uniquindio.co {
+
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
-    opens co to javafx.fxml;
-    exports co;
+    opens co.edu.uniquindio.proyectoFinal to javafx.fxml;
+    exports co.edu.uniquindio.proyectoFinal;
 }
